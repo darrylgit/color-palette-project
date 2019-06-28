1b4a40305b9315fae48a8622ef0e0da3b12885cb
darrylgit <darrylsmithup@gmail.com>
2019-06-28 07:16:29 -0400

    Updates

---
+++ precache-manifest.ad895baee8ce6916bcd652920baa86f8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fad934dff3ae82ea056eb9840974d7",
+    "url": "/color-palette-project/index.html"
+  },
+  {
+    "revision": "ba3a7e9aac7b86db51da",
+    "url": "/color-palette-project/static/css/main.6c5e5f12.chunk.css"
+  },
+  {
+    "revision": "ec4873e76ca3558e1dba",
+    "url": "/color-palette-project/static/js/2.084862a4.chunk.js"
+  },
+  {
+    "revision": "ba3a7e9aac7b86db51da",
+    "url": "/color-palette-project/static/js/main.bd0fd8fd.chunk.js"
+  },
+  {
+    "revision": "7453f069a46313040051",
+    "url": "/color-palette-project/static/js/runtime~main.86dbde41.js"
+  },
+  {
+    "revision": "d815b0a29adf3450c55f56e2fb813be4",
+    "url": "/color-palette-project/static/media/Montserrat-Medium.d815b0a2.otf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "/color-palette-project/static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/color-palette-project/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/color-palette-project/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "58e05c4c38f4595190a8c83f68b09337",
+    "url": "/color-palette-project/static/media/Oxygen.58e05c4c.otf"
+  },
+  {
+    "revision": "f44235f9000cd3d8d57738bb2eaf01e1",
+    "url": "/color-palette-project/static/media/WorkSans-Regular.f44235f9.otf"
+  }
+]);
